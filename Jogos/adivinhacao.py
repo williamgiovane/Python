ef7e85bf5851564b4e061bffd0e9de2983b2b706
William Giovani Testoni <williamgiovane1@gmail.com>
2021-04-27 13:03:59 -0300

    [ALT] Adicionando while
    Aula 04 - Alura
    Trabalhando com repetição

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -2,22 +2,32 @@ print("\n*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
+#Resposta
 numero_secreto = 42
+total_tentativas = 1
+chute = 0
 
-chute = int(input("\nDigite o seu número: "))
-print("\nVocê digitou: ", chute)
+while ((chute != numero_secreto) and (total_tentativas <= 3)):
+    print("\nTentativa: ", total_tentativas, " de 3")
 
-#Condições para o if
-acertou = chute == numero_secreto 
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    #Pergunta para o usuario
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou: ", chute)
 
-if (acertou):
-    print("\nVocê acertou!")
-else:
-    if (maior):
-        print ("\nVocê errou, o seu chute foi maior que o numero secreto")
-    elif (menor):
-        print ("\nVocê errou, o seu chute foi menor que o numero secreto")
+    #Condições para o if
+    acertou = chute == numero_secreto 
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    #Testas condições
+    if (acertou):
+        print("\nVocê acertou!")
+    else:
+        if (maior):
+            print ("\nVocê errou, o seu chute foi maior que o numero secreto")
+        elif (menor):
+            print ("\nVocê errou, o seu chute foi menor que o numero secreto")
+
+    total_tentativas = total_tentativas + 1
 
 print("\nFim do jogo")
