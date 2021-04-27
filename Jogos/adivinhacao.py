f8f0ec41df1f3bfa39ff83bd497ce3af73c4c538
William Giovani Testoni <williamgiovane1@gmail.com>
2021-04-27 12:42:51 -0300

    [ALT] Adicionando condições
    Aula 03.1 - Alura
    A condição elif

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -7,9 +7,17 @@ numero_secreto = 42
 chute = int(input("\nDigite o seu número: "))
 print("\nVocê digitou: ", chute)
 
-if (numero_secreto == chute):
+#Condições para o if
+acertou = chute == numero_secreto 
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
     print("\nVocê acertou!")
 else:
-    print("\nVocê errou!")
+    if (maior):
+        print ("\nVocê errou, o seu chute foi maior que o numero secreto")
+    elif (menor):
+        print ("\nVocê errou, o seu chute foi menor que o numero secreto")
 
 print("\nFim do jogo")
