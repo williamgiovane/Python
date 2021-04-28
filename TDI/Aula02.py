8151e36f2ee8e47ede4813269a134144a8047893
William Giovani Testoni <williamgiovane1@gmail.com>
2021-04-27 21:31:31 -0300

    [ALT] Define de functions
    Format e define de functions

---
+++ TDI/Aula02.py
@@ -0,0 +1,10 @@
+def ataque_ping(ip, porta):
+    print("Atacando o {} na porta {}".format(ip, porta))
+
+ataque_ping("192.168.0.1", "80")
+
+
+def calc(n1,n2):
+    return print(n1 + n2)
+
+calc(10,5)
