bdc506ee1f2620870dd9878c967c15cca9b1c831
William Giovani Testoni <williamgiovane1@gmail.com>
2021-05-05 16:08:48 -0300

    [ALT] Começando o jogo da forca

--- Alura/Jogos/forca.py
+++ Alura/Jogos/forca.py
@@ -4,9 +4,18 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
 
-    while():
-        input()
+    while((enforcou != True) and (acertou != True)):
+        chute = input("Qual letra?\n")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            
+            index = index + 1
     
     print("\nFim do jogo!")
 
