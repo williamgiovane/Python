ec5bfb8d265558596cd17f96a1f4482448885a22
William Giovani Testoni <williamgiovane1@gmail.com>
2021-05-05 14:23:12 -0300

    [ALT] Mesmo tratamento para escolha

--- Jogos/jogos.py
+++ Jogos/jogos.py
@@ -1,13 +1,17 @@
 import adivinhacao
 import forca
 
-print("\n*********************************")
-print("*******Escolha o seu jogo!*******")
-print("*********************************")
+def escolhe_jogo():
+    print("\n*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
 
-jogo = int(input("Qual jogo você quer jogar? \n(1) Forca (2) Adivinhação\n"))
+    jogo = int(input("Qual jogo você quer jogar? \n(1) Forca (2) Adivinhação\n"))
 
-if(jogo == 1):
-    forca.jogar()
-elif(jogo == 2):
-    adivinhacao.jogar()
+    if(jogo == 1):
+        forca.jogar()
+    elif(jogo == 2):
+        adivinhacao.jogar()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()
