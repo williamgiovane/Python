a0335539e30ea938d8ab13727d089488fcc450ee
William Giovani Testoni <williamgiovane1@gmail.com>
2021-05-05 20:21:06 -0300

    [ALT] Tratamento para tentivas

--- Alura/Jogos/forca.py
+++ Alura/Jogos/forca.py
@@ -3,33 +3,40 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "Banana"
+    palavra_secreta = "Banana".lower()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     print("\nA palavra contem {} letras".format(len(letras_acertadas)))
 
     enforcou = False
     acertou = False
+    tentivas = 1
 
     while((enforcou != True) and (acertou != True)):
         chute = input("\nQual letra?\n").lower().strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute == letra.lower()):
-                letras_acertadas[index] = letra
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
 
-            index = index + 1
+                index += 1
 
-        print("\nPalavra:", letras_acertadas)
+            print("\nPalavra:", letras_acertadas)
 
-        letras_restantes = int(letras_acertadas.count('_'))
+            letras_restantes = int(letras_acertadas.count('_'))
 
-        if(letras_restantes != 0):
-            print("\nAinda faltam acertar {} letras".format(letras_restantes))
+            if(letras_restantes == 0):
+                print("Parabéns você acertou a palavra!")
+                acertou = True
+            else:
+                print("\nAinda faltam acertar {} letras".format(letras_restantes))
         else:
-            print("Parabéns você acertou a palavra!")
-            acertou = True
+            if(tentivas == 6):
+                enforcou = True
+            else:
+                tentivas += 1
 
     print("\nFim do jogo!")
 
