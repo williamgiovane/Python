ab84334cc090a86f811d3564644773ccb95d94b3
William Giovani Testoni <williamgiovane1@gmail.com>
2021-05-05 15:48:46 -0300

    [ALT] Caminho

--- Alura/Jogos/forca.py
+++ Alura/Jogos/forca.py
@@ -3,6 +3,11 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    while():
+        input()
+    
     print("\nFim do jogo!")
 
 if(__name__ == "__main__"):
