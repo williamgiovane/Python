240dde805ab90f952468d68ed3a8b4199d10f708
William Giovani Testoni <williamgiovane1@gmail.com>
2021-05-05 19:14:47 -0300

    [ALT] Tratamento da string
    Adição da method com o lower() para tornar tudo minusculo no chute e no tratamento da palavra secreta e foi usada o method strip() para remover os espaços em branco

--- Alura/Jogos/forca.py
+++ Alura/Jogos/forca.py
@@ -3,16 +3,16 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "Banana"
     enforcou = False
     acertou = False
 
     while((enforcou != True) and (acertou != True)):
-        chute = input("Qual letra?\n")
+        chute = input("Qual letra?\n").lower().strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
+            if(chute == letra.lower()):
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             
             index = index + 1
